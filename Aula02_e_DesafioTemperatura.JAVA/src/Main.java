81eaffbc345d970e5a0d51a3432ab703cfdbe50a
Lucas José Lima <100000029+Lucasjlima@users.noreply.github.com>
2024-09-10 20:08:31 -0300

    Add files via upload

---
+++ Aula02_e_DesafioTemperatura.JAVA/src/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Condicionais()
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite sua idade: ");
+        var idade = scanner.nextInt();
+        scanner.nextLine();
+
+            if(idade >= 18 && idade <= 200)
+            System.out.println("A pessoa é maior de idade");
+            else if (idade > 200);
+            System.out.println("A pessoa é um vampiro");
+
+
+
+
+
+    }
+}
